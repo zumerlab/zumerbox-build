b3b8d571fa0eff711dc181a6f0410cac38bf5831
Juan Martin <martinmuda@gmail.com>
2024-03-27 14:55:25 +0300

    feat: add options to bundle code

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # @zumerbox/build
 
-A lightweight build tool for compiling and bundling your project assets.
+The Zumerbox build tool is a command-line utility designed to bundle and compile JavaScript and SCSS files into distributable formats. It leverages the esbuild library for efficient bundling and minification, providing developers with a streamlined workflow for frontend development.
+
+Refer to the [ZumerBox bundle](https://github.com/zumerlab/zumerbox) for more information and tools.
 
 ## Installation
 
@@ -8,11 +10,31 @@ A lightweight build tool for compiling and bundling your project assets.
 npm install @zumerbox/build --save-dev
 ```
 
-## Usage
+## Usage:
 
 ```bash
-npx @zumerbox/build
+npx @zumerbox/build --name <bundler_name> --js <path_to_js_file> --scss <path_to_scss_file> [options]
 ```
 
-Refer to the [ZumerBox bundle](https://github.com/zumerlab/zumerbox) for more information.
-```
+## Options:
+
+- `--name (-n)`: Specifies the name of the bundler. If not provided, it will attempt to use the name from the package.json file. If the name is not available in package.json, it will default to "MyBundler".
+- `--js (-j)`: Specifies the path to the JavaScript file (optional).
+- `--scss (-s)`: Specifies the path to the SCSS file (optional).
+- `--minify (-m)`: Generates minified output files (JavaScript and CSS) if enabled.
+- `--outdir (-d)`: Specifies the destination folder for the output files. Default is "dist".
+- `--platform (-p)`: Specifies the target platform for esBuild. Default is "browser".
+- `--help (-h)`: Displays the help message.
+
+
+## Functionality:
+- **Input files**: Specify the JavaScript and SCSS files to be bundled using the command-line options.
+- **Bundling**: The tool utilizes esbuild to bundle the provided JavaScript and SCSS files into distributable formats.
+- **Minification**: Optionally, the tool can minify the output files by setting the '--minify' flag.
+- **Package information**: The tool reads package.json from the project directory to extract metadata such as name, version, author, and license. This information is included as a banner in the output files.
+- **Output directory**: Output files are saved to the specified output directory ('dist' by default).
+- **Error handling**: The tool gracefully handles errors during the bundling process and exits with an error code if an error occurs.
+
+## Credits:
+
+This tools is powered by esbuild (https://esbuild.github.io/)
--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const esbuild = require('esbuild')
-const path = require('path')
-const yargs = require('yargs')
-const sassPlugin = require('esbuild-plugin-sass')
+const esbuild = require('esbuild');
+const path = require('path');
+const yargs = require('yargs');
+const sassPlugin = require('esbuild-plugin-sass');
 
 // Define command-line options using yargs
 const argv = yargs
@@ -11,24 +11,21 @@ const argv = yargs
   .option('name', {
     alias: 'n',
     describe: 'Name of the bundler',
-    demandOption: true,
     type: 'string'
   })
   .option('js', {
     alias: 'j',
     describe: 'Path to JavaScript file',
-    demandOption: true,
     type: 'string'
   })
   .option('scss', {
     alias: 's',
     describe: 'Path to SCSS file',
-    demandOption: true,
     type: 'string'
   })
   .option('minify', {
     alias: 'm',
-    describe: 'Generate minified output files (JavaScript and CSS)',
+    describe: 'Generate minified output files',
     type: 'boolean',
     default: false
   })
@@ -38,46 +35,63 @@ const argv = yargs
     type: 'string',
     default: 'dist'
   })
+  .option('platform', {
+    alias: 'p',
+    describe: 'Target platform for esBuild (e.g., browser, node)',
+    type: 'string',
+    default: 'browser'
+  })
   .help('h')
-  .alias('h', 'help').argv
+  .alias('h', 'help').argv;
 
 // Read package.json from project directory
-const packageJsonPath = path.resolve(__dirname, '../package.json')
-const pkg = require(packageJsonPath)
+const packageJsonPath = path.resolve(__dirname, '../package.json');
+const pkg = require(packageJsonPath);
 
-// Extract name from package.json
-const bundlerName = pkg.name || 'MyBundler'
+// Use package name from package.json if no name provided
+const bundlerName = argv.name || pkg.name || 'MyBundler';
+
 const setBanner = () => `
 /*
 * ${pkg.name}
 * v.${pkg.version}
 * Author ${pkg.author}
 * License ${pkg.license}
-**/`
+**/`;
 
 // Configuration for esbuild
 const options = {
-  entryPoints: [argv.scss, argv.js],
-  entryNames: `${argv.name}`,
+  entryPoints: [],
+  entryNames: bundlerName,
   bundle: true,
   banner: {
     js: setBanner(),
     css: setBanner()
   },
-  outdir: `${argv.outdir}`,
+  outdir: argv.outdir,
   metafile: true,
-  plugins: [sassPlugin()]
+  plugins: [sassPlugin()],
+  platform: argv.platform
+};
+
+if (argv.js) {
+  options.entryPoints.push(argv.js);
+}
+
+if (argv.scss) {
+  options.entryPoints.push(argv.scss);
 }
 
 const optionsMinify = {
-  entryPoints: [argv.scss, argv.js],
-  entryNames: `${argv.name}.min`,
-  outdir: `${argv.outdir}`,
+  entryPoints: options.entryPoints,
+  entryNames: `${bundlerName}.min`,
+  outdir: argv.outdir,
   bundle: true,
   metafile: true,
   minify: true,
-  plugins: [sassPlugin()]
-}
+  plugins: [sassPlugin()],
+  platform: argv.platform
+};
 
 // Run esbuild
 esbuild
@@ -87,5 +101,5 @@ esbuild
       esbuild.build(optionsMinify)
     : console.log('⚡ Minify skipped ⚡')
   )
-  .then(() => console.log('⚡ Styles & Scripts Compiled! ⚡ '))
-  .catch(() => process.exit(1))
+  .then(() => console.log('⚡ Files compiled! ⚡ '))
+  .catch(() => process.exit(1));
